60d152c3e7a2b9f2a98160769b7bad0660ed9edd
Gabriel <gabrieljacg13@gmail.com>
2026-09-12 17:42:36 -0500

    feat: Creando la clase DatabadeHelper para leer e inicializar productos.sql

---
+++ app/src/main/java/db/DatabaseHelper.kt
@@ -0,0 +1,23 @@
+package db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(
+    context,
+    "ventas.db",
+    null,
+    1
+) {
+    override fun onCreate(db: SQLiteDatabase) {
+
+    }
+    override fun onUpgrade(
+        db: SQLiteDatabase,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+
+    }
+}
