8b498c4ec0524c125ecc4d4585254f9829c44f72
Gabriel <gabrieljacg13@gmail.com>
2026-09-12 18:34:40 -0500

    feat: agregamos una funcion que nos permita sustrar los datoo de productos.sql y lo transforme en string, luego dentro de la funcion onCreate creamos una variable sql que tendra el string que le dio la funcion leerSQL despues usamos filtros que nos permitan leer el string de manera que sean solo sentencias y una por una

--- app/src/main/java/db/DatabaseHelper.kt
+++ app/src/main/java/db/DatabaseHelper.kt
@@ -1,23 +1,39 @@
 package db
 
+
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper(context: Context) : SQLiteOpenHelper(
+class DatabaseHelper(private val context: Context) : SQLiteOpenHelper(
     context,
     "ventas.db",
     null,
     1
 ) {
+
     override fun onCreate(db: SQLiteDatabase) {
+        val sql = leerSQL(context)
+        val sentencias = sql.split(";")
+        for (sentencia in sentencias) {
+            if (sentencia.trim().isNotEmpty()) {
+                db.execSQL(sentencia)
+            }
+        }
 
     }
+
     override fun onUpgrade(
         db: SQLiteDatabase,
         oldVersion: Int,
         newVersion: Int
     ) {
 
+    }
+
+    private fun leerSQL(context: Context): String {
+        return context.assets.open("productos.sql").bufferedReader().use {
+            it.readText()
+        }
     }
 }
