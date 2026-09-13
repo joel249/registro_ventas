5546efbf7130184816754b21d0e5c7778c6a0d4e
Gabriel <gabrieljacg13@gmail.com>
2026-09-12 23:47:02 -0500

    feat: mostrar ventas de SQLite en tarjetas

--- app/src/main/java/com/example/android_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/android_sqlite/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.android_sqlite
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import db.DatabaseHelper
-import android.util.Log
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,33 +21,93 @@ class MainActivity : AppCompatActivity() {
         val dbHelper = DatabaseHelper(this)
         val db = dbHelper.writableDatabase
 
-        Log.d("BD", "Base de datos conectada")
+        val contenedorVentas = findViewById<LinearLayout>(
+            R.id.contenedor_ventas
+        )
 
-        val cursor = db.rawQuery("SELECT * FROM ventas", null)
+        val cursor = db.rawQuery(
+            "SELECT * FROM ventas",
+            null
+        )
+
         while (cursor.moveToNext()) {
 
-            val codigo = cursor.getString(cursor.getColumnIndexOrThrow("codigo"))
-
-            val nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"))
-
-            val precio = cursor.getDouble(cursor.getColumnIndexOrThrow("precio"))
-
-            val cantidad = cursor.getInt(cursor.getColumnIndexOrThrow("cantidad"))
-
-            val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
-
-            val fechaVenta = cursor.getString(cursor.getColumnIndexOrThrow("fecha_venta"))
-
-            Log.d(
-                "VENTAS",
-                "Código: $codigo | Nombre: $nombre | Precio: $precio | Cantidad: $cantidad | Tipo: $tipo | Fecha: $fechaVenta"
+            val codigo = cursor.getString(
+                cursor.getColumnIndexOrThrow("codigo")
             )
 
+            val nombre = cursor.getString(
+                cursor.getColumnIndexOrThrow("nombre")
+            )
+
+            val precio = cursor.getDouble(
+                cursor.getColumnIndexOrThrow("precio")
+            )
+
+            val cantidad = cursor.getInt(
+                cursor.getColumnIndexOrThrow("cantidad")
+            )
+
+            val tipo = cursor.getString(
+                cursor.getColumnIndexOrThrow("tipo")
+            )
+
+            val fechaVenta = cursor.getString(
+                cursor.getColumnIndexOrThrow("fecha_venta")
+            )
+
+            // Crear una tarjeta usando item_venta.xml
+            val tarjeta = LayoutInflater.from(this).inflate(
+                R.layout.item_venta,
+                contenedorVentas,
+                false
+            )
+
+            // Buscar los TextView de la tarjeta
+            val tvTipo = tarjeta.findViewById<TextView>(R.id.tv_tipo)
+            val tvCodigo = tarjeta.findViewById<TextView>(R.id.tv_codigo)
+            val tvNombre = tarjeta.findViewById<TextView>(R.id.tv_nombre)
+            val tvPrecio = tarjeta.findViewById<TextView>(R.id.tv_precio)
+            val tvCantidad = tarjeta.findViewById<TextView>(R.id.tv_cantidad)
+            val tvFecha = tarjeta.findViewById<TextView>(R.id.tv_fecha)
+
+            // Colocar los datos de SQLite en la tarjeta
+            tvTipo.text = tipo.uppercase()
+            tvCodigo.text = "Código: $codigo"
+            tvNombre.text = "Producto: $nombre"
+            tvPrecio.text = "Precio: S/ $precio"
+            tvCantidad.text = "Cantidad: $cantidad"
+            tvFecha.text = "Fecha: $fechaVenta"
+
+            // Cambiar color según el tipo de venta
+            if (tipo == "factura") {
+                tvTipo.setTextColor(Color.RED)
+            } else if (tipo == "boleta") {
+                tvTipo.setTextColor(Color.GREEN)
+            }
+
+            // Agregar la tarjeta al contenedor
+            contenedorVentas.addView(tarjeta)
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        cursor.close()
+        db.close()
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.main)
+        ) { v, insets ->
+
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+            )
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
             insets
         }
     }
