f2259686b8d92b55e03d218ec2992ef5fbc9c2d5
Gabriel <gabrieljacg13@gmail.com>
2026-09-12 22:01:13 -0500

    feat conecta DatabaseHelper con MainActivity

--- app/src/main/java/com/example/android_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/android_sqlite/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import db.DatabaseHelper
+import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val dbHelper = DatabaseHelper(this)
+        val db = dbHelper.writableDatabase
+
+        Log.d("BD", "Base de datos conectada")
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/db/DatabaseHelper.kt
+++ app/src/main/java/db/DatabaseHelper.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+
 class DatabaseHelper(private val context: Context) : SQLiteOpenHelper(
     context,
     "ventas.db",
